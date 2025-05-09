1d5be0a911e95d734b3b1f36be81863bcdb2ab3a
lsgyansys <litton.sinha@gyansys.com>
2025-05-09 22:31:09 +0530

    added for multiplication

--- src/test/java/gyansysLandingPage/LaunchGyanSysSite.java
+++ src/test/java/gyansysLandingPage/LaunchGyanSysSite.java
@@ -8,5 +8,8 @@ public class LaunchGyanSysSite
 		int b = 20;
 		int c = a + b;
 		System.out.println("Sum of a and b is: " + c);
+		
+		int d = a * b;
+		System.out.println("Multiplication of a and b is: " + d);
 	}
 }
