126c0e1622509a3b6192b7613b90b099b63ef56f
lsgyansys <litton.sinha@gyansys.com>
2025-05-09 22:05:02 +0530

    first check

---
+++ src/test/java/gyansysLandingPage/LaunchGyanSysSite.java
@@ -0,0 +1,12 @@
+package gyansysLandingPage;
+
+public class LaunchGyanSysSite 
+{
+	public static void main (String args[])
+	{
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println("Sum of a and b is: " + c);
+	}
+}
